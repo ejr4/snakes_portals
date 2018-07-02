f415f814696e2d605994a15a6b41401483cf5d7c
Eric <ejr4@pm.me>
2018-07-01 23:05:14 -0700

    another snake is here and about to get a helper

--- ProgramTiles.js
+++ ProgramTiles.js
@@ -14,12 +14,16 @@ class ProgramTiles extends Phaser.Scene {
         this.load.image('ladder', 'assets/ladder.png');
         this.load.image('snake', 'assets/snake.png');
         this.load.image('snake256', 'assets/snake256.png');
+        this.load.image('snakeY', 'assets/snakeY.png');
     }
 
     create() {
-        var helpText = this.add.text(640 + 10, 64, 'L - snake \n scaleX up', { fontSize: '16px', fill: '#909' });
-        var helpText = this.add.text(640 + 10, 128, 'M - snake \n scaleX down', { fontSize: '16px', fill: '#862' });
-        var helpText = this.add.text(640 + 10, 192, 'G - snake \n angle up', { fontSize: '16px', fill: '#D54' });
+        var helpTextL = this.add.text(640 + 10, 64, 'L - snake \n scaleX up', { fontSize: '16px', fill: '#909' });
+        var helpTextM = this.add.text(640 + 10, 128, 'M - snake \n scaleX down', { fontSize: '16px', fill: '#862' });
+        var helpTextG = this.add.text(640 + 10, 192, 'G - snake \n angle up', { fontSize: '16px', fill: '#D54' });
+        var helpTextY = this.add.text(640 + 10, 240, 'Y - snake256 \n angle up', { fontSize: '16px', fill: '#E2E' });
+        var helpTextU = this.add.text(640 + 10, 300, 'U - snake256 \n angle up', { fontSize: '16px', fill: '#19A' });
+        var helpTextY = this.add.text(640 + 10, 300, 'Y - snakeY \n angle up', { fontSize: '16px', fill: '#19A' });
 
        this.tiles = [];
         for(let i = 0; i < 10; i++){
@@ -37,6 +41,17 @@ class ProgramTiles extends Phaser.Scene {
         var player2 = this.add.image(32*10 + 48, 320 + 48, 'blueguy');// 
         var ladder = this.add.image(32*10 + 48, 320 + 64 + 48, 'ladder');// 
         var snake = this.add.image(32*2 + 48, 320 + 48, 'snake');// 
+        var snake256 = this.add.image(64*4 + 32, 64*3 + 32, 'snake256');// 
+        snake256.originX = 0;
+        snake256.originY = 0;
+        snake256.displayOriginX = 0;
+        snake256.displayOriginY = 0;
+        //
+        var snakeY = this.add.image(64*4 + 32, 64*3 + 32, 'snakeY'); 
+        snakeY.originX = 0;
+        snakeY.originY = 0;
+        snakeY.displayOriginX = 0;
+        snakeY.displayOriginY = 0;
 
         // Left
         this.input.keyboard.on('keydown_A', function (event) {
@@ -58,9 +73,18 @@ class ProgramTiles extends Phaser.Scene {
                 player.y += 32;
                 player.angle = 90;
         });
+        //  Snake2 rotate up
         //  Snake rotate up
         this.input.keyboard.on('keydown_G', function (event) {
                 snake.angle += 22.5;
+                
+        });
+        this.input.keyboard.on('keydown_Y', function (event) {
+                snakeY.angle += 3;
+                
+        });
+        this.input.keyboard.on('keydown_U', function (event) {
+                snake256.angle += 22.5;
                 
         });
         //  Snake scaleX up
