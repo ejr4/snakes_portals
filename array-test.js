654304d1f2263440cbde2bcb943fc02bd5e36626
Eric <ejr4@pm.me>
2018-07-07 22:54:35 -0700

    some kinda bug in array

--- array-test.js
+++ array-test.js
@@ -1,29 +1,39 @@
 // idea: simple functions give next state.  animation follows by animate_to.  however may not need for proof of concept.
 var log = console.log;
-var board1 = [ , , , ,7, , , , ,   ]  // - 1 is end, perhaps
-var board2 = [ , , , , , , , 3 , ,   ]  // - 1 is end, perhaps
-var board3 = [ , , 6, , , , , 1, , ]
-var player1tile = 0,
-    player2tile = 0,
-    player3tile = 0,
-    loopcount = 0;
+var board1 = [ , , , ,7, , , , ,   , 10] ; // - 1 is end, perhaps
+var board2 = [ , , , , , , , 3 , ,   , 10];  // - 1 is end, perhaps
+var board3 = [ , , 6, , , , , 1, , , 10];
+// var player1tile = 0,
+//     player2tile = 0,
+//     player3tile = 0,
+//     loopcount = 0;
+var players = [0, 2, 7];
 // while(player1tile != 10 && loopcount < 5) {
 //     player1tile = board[player1tile] || player1tile + 1;
 //     console.log( player1tile);
 // }
 
-function runboard(board, playertile) { 
+function runboard(board, players) { 
+    var record = [];
     let steps = 0;
-    while(playertile != 10 && ++steps < 30) {
-        playertile = board[playertile] || playertile + 1;
-        console.log( playertile);
+    
+    record.push(players);
+    log(record);
+    while(++steps < 10) {
+        let newstate = players.map((player) => { 
+            // (board[player] || player + 1)
+            player + 1
+        });
+        record.push(newstate);
+        console.log(newstate);
     }
+    return record;
 }
-log("ladder board:" )
-runboard(board1,player1tile);
-log("snake board:" );
-runboard(board2,player2tile);
-log("inf hourglass board:" );
-runboard(board3, player3tile);
+// log("ladder board:" )
+// runboard(board1,player1tile);
+// log("snake board:" );
+// runboard(board2,player2tile);
+log("inwhowhat:" );
+runboard(board3, players);
 
 
